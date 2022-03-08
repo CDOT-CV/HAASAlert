de732011b551dba88d65fd81418253c37d714052
Michael7371 <michael@neaera.net>
2022-03-08 12:28:17 -0700

    fixes for cloud function deployment

--- haas_websocket/token_regenerate_CF.py
+++ haas_websocket/token_regenerate_CF.py
@@ -1,7 +1,7 @@
-from haas_websocket.haas_rest_handler import TokenAuth
+from haas_rest_handler import TokenAuth
 
 # Will run on an interval in GCP as a cloud function to regenerate the bearer token each day
 
-rest_agent = TokenAuth()
-
-rest_agent.refreshToken()
+def refresh_token(request):
+    rest_agent = TokenAuth()
+    rest_agent.refreshToken()
